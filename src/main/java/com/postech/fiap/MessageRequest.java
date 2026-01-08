5d94e91bb73966a87e4ad201b21110d71304b4c6
IsraelJMameli <israel.mameli@gmail.com>
2026-01-07 21:19:49 -0300

    Problemas 404/limpeza pom

--- src/main/java/com/postech/fiap/MessageRequest.java
+++ src/main/java/com/postech/fiap/MessageRequest.java
@@ -1,16 +1,19 @@
 package com.postech.fiap;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@RegisterForReflection
 public class MessageRequest {
     private Message message;
     private String subscription;
 
     @Getter
     @Setter
+    @RegisterForReflection
     public static class Message {
         private String data;
     }
